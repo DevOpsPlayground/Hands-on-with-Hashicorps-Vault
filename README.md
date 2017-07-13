08738b61d49542474ef5ac845e1d2bd2c46fb12f
Mourad <mourad@ECS0546.local>
2017-07-13 13:24:19 +0100

    added Mysql reading + vault api call

--- README.md
+++ README.md
@@ -1,19 +1,28 @@
 # devopsplayground13-vault
 All the content needed for the DevOps Playground Meetup on Vault (13) will be worked on here.
 
+## done on the box
+sudo apt-get install mysql mysql-server
+mysqladmin -u root password 'root'
+mysql secure_installation
+mysql > create database Playground;
+mysql > use Playground
+mysql > create table main (id Varchar(120));
+insert some data into this id row
+
 ## Step 0 - Setting up Vault Server
 vault server -dev
 export VAULT_ADD=http://127.0.0.1:8200
 copy root token
- 
+export VAULT_TOKEN= <token>
+
 
 ## Step 1 - our first secret
 vault write secret/application url= ecs-digital.co.uk
 vault read  secret/application
 vault read -field=url secret/application
 
-
-## Step 2 - inject our first secret
+## Step 2 - inject our first secret with EnvConsul
 
 vim config
   vault {
--- app.py
+++ app.py
@@ -1,5 +1,54 @@
-import os
+import os, MySQLdb, requests, logging, json
 
-envName = str(os.environ['NAME'])
-print envName
- 
+mysql = ""
+
+def getEnvVar():
+    global vault_addr
+    global vault_token
+    vault_token = str(os.environ['VAULT_TOKEN'])
+    vault_addr = str(os.environ['VAULT_ADDR'])
+    print vault_addr," --- ", vault_token
+    print "\n"
+    return vault_addr, vault_token
+
+def parseDB(host, db, user, passwd):
+    #connect to the db existing on the machine
+    #db = MySQLdb.connect(host="localhost", user="root", passwd="root",  db="Playground")
+    global mysql
+    mysql = MySQLdb.connect(host=host, user=user, passwd=passwd,  db=db)
+    cur = mysql.cursor()
+    # Read all from main tables
+    f=open('list.txt','r')
+    for line in f:
+        cur.execute("INSERT INTO main (id) VALUES (\'"+line+"\')")
+
+    # cur.execute("SELECT * FROM main")
+    # for row in cur.fetchall():
+    #     print row[0]
+    mysql.close()
+
+def getDBCredsFromVault(vault_addr, vault_token):
+    headers = '{X-Vault-Token:', vault_token,'}'
+    Resp = requests.get(vault_addr + "/v1/secret/database", headers = {"X-Vault-Token": vault_token})
+
+    # 200 = OK, other = NOK
+    if(Resp.ok):
+        secret = json.loads(Resp.content)
+        # print secret
+        print "host : ", secret["data"]["host"]
+        host = secret["data"]["host"]
+        print "database : ", secret["data"]["db"]
+        db = secret["data"]["db"]
+        print "user : ", secret["data"]["user"]
+        user = secret["data"]["user"]
+        print "password : ", secret["data"]["passwd"]
+        passwd = secret["data"]["passwd"]
+        print "\n"
+    else:
+        Resp.raise_for_status()
+    return host, db, user, passwd
+
+#### main
+vault_addr, vault_token = getEnvVar()
+host, db, user, passwd = getDBCredsFromVault( vault_addr, vault_token)
+parseDB(host, db, user, passwd)
