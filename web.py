9765ef78bf12d6d76591603346121e03f2fa17d0
Mourad <mourad@ECS0546.local>
2017-07-19 11:35:11 +0100

    fixed issue when populating db, add simple flask, still one big ugly file

--- web.py
+++ web.py
@@ -1,7 +1,8 @@
-import os, MySQLdb, requests, logging, json
+import os, MySQLdb, requests, json
+from flask import Flask
+app = Flask(__name__)
 
 mysql = ""
-
 def getEnvVar():
     global vault_addr
     global vault_token
@@ -11,20 +12,18 @@ def getEnvVar():
     print "\n"
     return vault_addr, vault_token
 
-def parseDB(host, db, user, passwd):
+def populateDB(host, db, user, passwd):
     #connect to the db existing on the machine
     #db = MySQLdb.connect(host="localhost", user="root", passwd="root",  db="Playground")
     global mysql
-    mysql = MySQLdb.connect(host=host, user=user, passwd=passwd,  db=db)
+    mysql = MySQLdb.connect(host=host, user=user, passwd=passwd,  db="Playground")
     cur = mysql.cursor()
     # Read all from main tables
     f=open('list.txt','r')
     for line in f:
         cur.execute("INSERT INTO main (id) VALUES (\'"+line+"\')")
 
-    # cur.execute("SELECT * FROM main")
-    # for row in cur.fetchall():
-    #     print row[0]
+    mysql.commit()
     mysql.close()
 
 def getDBCredsFromVault(vault_addr, vault_token):
@@ -48,7 +47,30 @@ def getDBCredsFromVault(vault_addr, vault_token):
         Resp.raise_for_status()
     return host, db, user, passwd
 
+
+def readDB(host, db, user, passwd):
+    #connect to the db existing on the machine
+    #db = MySQLdb.connect(host="localhost", user="root", passwd="root",  db="Playground")
+    global mysql
+    mysql = MySQLdb.connect(host=host, user=user, passwd=passwd,  db=db)
+    cur = mysql.cursor()
+    # Read all from main tables
+
+    cur.execute("SELECT * from main")
+    results = cur.fetchall()
+    print results
+    # for row in results:
+    #     print row[0]
+    mysql.close()
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
 #### main
 vault_addr, vault_token = getEnvVar()
-host, db, user, passwd = getDBCredsFromVault( vault_addr, vault_token)
-parseDB(host, db, user, passwd)
+host, db, user, passwd = getDBCredsFromVault(vault_addr, vault_token)
+populateDB(host, db, user, passwd)
+readDB(host, db, user, passwd)
+hello_world()
